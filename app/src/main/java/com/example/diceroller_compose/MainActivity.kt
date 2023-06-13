6c0539bae0424d37ad6825aa1f84aa8c97ebd186
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-14 01:42:27 +0600

    Add an images

--- app/src/main/java/com/example/diceroller_compose/MainActivity.kt
+++ app/src/main/java/com/example/diceroller_compose/MainActivity.kt
@@ -3,16 +3,23 @@ package com.example.diceroller_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller_compose.ui.theme.DiceRollerComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,23 +27,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerComposeTheme {
-               DiceWithButtonAndImage(modifier = Modifier
-                   .fillMaxSize()
-                   .wrapContentSize(Alignment.Center))
+                DiceWithButtonAndImage(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center)
+                )
             }
         }
     }
 }
+
 @Preview
 @Composable
-fun DiceRollerApp(){
+fun DiceRollerApp() {
     DiceWithButtonAndImage()
 }
 
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
-    Column(modifier = modifier,
-    horizontalAlignment = Alignment.CenterHorizontally) {
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(R.drawable.dice_1), contentDescription = "1")
+        Spacer(modifier = Modifier.height(16.dp))
+    Button(onClick = { /*TODO*/ }) {
+        Text(stringResource(R.string.roll))
+        
+    }
+
     }
 }
