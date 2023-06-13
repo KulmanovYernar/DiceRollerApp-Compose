48af7875bbe81c4d21b2ac49b666667f54cbdace
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-14 01:35:28 +0600

    initial commit

---
+++ app/src/main/java/com/example/diceroller_compose/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.diceroller_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller_compose.ui.theme.DiceRollerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerComposeTheme {
+               DiceWithButtonAndImage(modifier = Modifier
+                   .fillMaxSize()
+                   .wrapContentSize(Alignment.Center))
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage()
+}
+
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+    Column(modifier = modifier,
+    horizontalAlignment = Alignment.CenterHorizontally) {
+    }
+}
